775a72fcfc2fe1b7392ac41cfd2e18d79460115c
shohbek <jalilovshohbek9909@gmail.com>
2024-09-18 18:02:00 +0500

    fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,9 +3,11 @@ import "./Navbar.scss";
 import { Link, NavLink } from "react-router-dom";
 // image
 import logo from "../../assets/shared/desktop/logo.svg";
-import menu from "../../assets/shared/mobile/icon-hamburger.svg"
+import menu from "../../assets/shared/mobile/icon-hamburger.svg";
+import { act, useState } from "react";
 
 function Navbar() {
+  const [active, setActive] = useState(false);
   return (
     <header className="navbar">
       <div className="navbar-container container">
@@ -25,7 +27,27 @@ function Navbar() {
             </li>
           </ul>
         </div>
-        <img className="menu" src={menu} alt="" />
+        <img
+          className="menu"
+          src={menu}
+          alt=""
+          onClick={() => setActive(!active)}
+        />
+        {active && (
+          <div className="dropdown">
+            <ul>
+              <li>
+                <NavLink to="/">HOME</NavLink>
+              </li>
+              <li>
+                <NavLink to="/about">ABOUT US</NavLink>
+              </li>
+              <li>
+                <NavLink to="/create-plan">CREATE YOUR PLAN</NavLink>
+              </li>
+            </ul>
+          </div>
+        )}
       </div>
     </header>
   );
